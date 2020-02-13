94ab23da7442393db707c486ff701446c54d32eb
Sam <sammarston15@gmail.com>
2020-02-13 00:57:04 -0700

    added onchange to login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,21 +1,50 @@
-import React,{ Component, ChangeEvent } from "react";
+import React,{ Component, FormEvent } from "react";
+import axios from 'axios';
+import './login.css'
 
-const Login: React.FC = () => {
 
-    function handleChange(e: ChangeEvent<HTMLInputElement>) {
-        
-    }
 
-    return(
-        <div className='login-background'>
-            <div className="login-modal">
-                <input className='login-textbox' name='' value='' type="text" onChange={handleChange}/>
-                <input className='login-textbox' name='' value='' type="text" onChange={handleChange}/>
-                
-                <button>login</button>
-            </div>
-        </div>
-    )
+type state = {
+    username: string,
+    password: string
 }
 
-export default Login;
+export default class Login extends Component<{}, state> {
+    constructor(props: state) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+
+    handleChangeUsername = (e: React.FormEvent<HTMLInputElement>): void => this.setState({username: (e.target as HTMLInputElement).value})
+    handleChangePassword = (e: React.FormEvent<HTMLInputElement>): void => this.setState({password: (e.target as HTMLInputElement).value})
+
+
+    handleLogin = async () => {
+        try {
+            if (this.state.username && this.state.password) {
+                let login = await axios.post('/api/login', this.state);
+                
+            }
+        } catch (error) {
+            
+        }
+    }
+
+
+    render() { 
+        return(
+            <div className='login-page'>
+                <div className='login-modal'>
+                    <input placeholder='username' onChange={this.handleChangeUsername} type="text"/>
+                    <input placeholder="password" onChange={this.handleChangePassword} type="password"/>
+                    <button>login</button>
+                </div>
+            </div>
+        )
+    }
+}
