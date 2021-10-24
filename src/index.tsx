6d11339612d7e619ead9117c2112b4bca8126af6
taylor.woodman@domo.com <taylor.woodman@domo.com>
2021-10-24 09:01:15 -0600

    fix style

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./index.module.scss";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { App } from "./components/app/App";
