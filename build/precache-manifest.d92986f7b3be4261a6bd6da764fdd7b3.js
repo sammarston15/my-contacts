3569d4766632a032b0afd1608c9f142d8bab54e1
Sam Marston <sammarston15@gmail.com>
2021-11-09 15:32:11 -0700

    created build using 'yarn build'

---
+++ build/precache-manifest.d92986f7b3be4261a6bd6da764fdd7b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "734da5f001629bb2d2c2a10d91a20a0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46e72f2843c22cc3b91d",
+    "url": "/static/css/main.54b761b7.chunk.css"
+  },
+  {
+    "revision": "360bf61e6eb4cffe512b",
+    "url": "/static/js/2.c98618d9.chunk.js"
+  },
+  {
+    "revision": "7827aa7977f8a8cd8bc1f07476294571",
+    "url": "/static/js/2.c98618d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e72f2843c22cc3b91d",
+    "url": "/static/js/main.90c3cf2c.chunk.js"
+  },
+  {
+    "revision": "429a34b09c03f31a770a",
+    "url": "/static/js/runtime-main.a8e1ba7a.js"
+  }
+]);
